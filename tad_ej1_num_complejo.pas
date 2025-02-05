0c9f74c2cdc6d57abc07eac057aeefd2135e3b97
Sergio Cavero <sergio.cavero@urjc.es>
2025-02-05 19:27:57 +0100

    Refactor complex number operations to improve readability and consistency in output formatting

--- tad_ej1_num_complejo.pas
+++ tad_ej1_num_complejo.pas
@@ -9,8 +9,8 @@ var
 
 begin
     // Crear dos números complejos
-    c1 := CrearComplejo(3, 4);
-    c2 := CrearComplejo(1, 2);
+    CrearComplejo(c1, 3, 4);
+    CrearComplejo(c2, 1, 2);
 
     // Obtener y mostrar las partes reales e imaginarias
     parteReal := GetParteReal(c1);
@@ -25,26 +25,40 @@ begin
 
     // Sumar dos números complejos
     resultado := Sumar(c1, c2);
-    writeln('Suma: ', resultado.Real:0:2, ' + ', resultado.Imaginario:0:2, 'i');
+    parteReal := GetParteReal(resultado);
+    parteImaginaria := GetParteImaginaria(resultado);
+    writeln('Suma: ', parteReal:0:2, ' + ', parteImaginaria:0:2, 'i');
 
     // Restar dos números complejos
     resultado := Restar(c1, c2);
-    writeln('Resta:', resultado.Real:0:2, ' + ', resultado.Imaginario:0:2, 'i');
+    parteReal := GetParteReal(resultado);
+    parteImaginaria := GetParteImaginaria(resultado);
+    writeln('Resta: ', parteReal:0:2, ' + ', parteImaginaria:0:2, 'i');
 
     // Multiplicar dos números complejos
     resultado := Multiplicar(c1, c2);
-    writeln('Multiplicación: ', resultado.Real:0:2, ' + ', resultado.Imaginario:0:2, 'i');
+    parteReal := GetParteReal(resultado);
+    parteImaginaria := GetParteImaginaria(resultado);
+    writeln('Multiplicación: ', parteReal:0:2, ' + ', parteImaginaria:0:2, 'i');
 
     // Dividir dos números complejos
     resultado := Dividir(c1, c2);
-    writeln('División: ', resultado.Real:0:2, ' + ', resultado.Imaginario:0:2, 'i');
+    parteReal := GetParteReal(resultado);
+    parteImaginaria := GetParteImaginaria(resultado);
+    writeln('División: ', parteReal:0:2, ' + ', parteImaginaria:0:2, 'i');
 
     // Elevar un número complejo a una potencia
     resultado := Potencia(c1, 2);
-    writeln('Potencia: ', resultado.Real:0:2, ' + ', resultado.Imaginario:0:2, 'i');
+    parteReal := GetParteReal(resultado);
+    parteImaginaria := GetParteImaginaria(resultado);
+    writeln('Potencia: ', parteReal:0:2, ' + ', parteImaginaria:0:2, 'i');
 
     // Modificar las partes reales e imaginarias
     SetParteReal(c1, 5);
     SetParteImaginaria(c1, 6);
-    writeln('Nuevo c1: ', c1.Real:0:2, ' + ', c1.Imaginario:0:2, 'i');
+    parteReal := GetParteReal(c1);
+    parteImaginaria := GetParteImaginaria(c1);
+    writeln('Nueva parte real de c1: ', parteReal:0:2);
+
+
 end.
